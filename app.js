75d540efa5900d4f7ce04c0d68342df063998cf0
Henrique <henrique150789123@gmail.com>
2025-01-17 10:59:33 -0300

    adicionando a função break no loop

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 alert ('boas vindas ao jogo do numero secreto');
 let numeroSecreto = 5;
-console.log(numeroSecreto);
-let chute
+let chute;
+let tentativas = 1;
 
-while (chute != numeroSecreto){
+while (chute != numeroSecreto) {
     chute = prompt ('escolha um número entre 1 e 10');
 
-    if (numeroSecreto == chute) {
-        alert(`isso aí! você descobriu o número secreto: ${numeroSecreto}! Parabéns :)`);
+    if (chute == numeroSecreto) {
+        break;
     } else {
         if(chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
-            }    else{
-                alert(`O número secreto é maior que ${chute}`);
-            }
+        } else{
+            alert(`O número secreto é maior que ${chute}`);
         }
-    
-    
+        tentativas++;
+    }
 }
 
+if(tentativas == 1 ) {
+alert(`isso aí! você descobriu o número secreto: ${numeroSecreto} com ${tentativas} tentativa! Parabéns :)`);
+} else{
+    alert(`isso aí! você descobriu o número secreto: ${numeroSecreto} com ${tentativas} tentativas! Parabéns :)`);
+}
