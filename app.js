eb26d5155085b7749947c54bddf491a294c730ef
Henrique Gomes <60164654+riquegsilva@users.noreply.github.com>
2025-01-16 21:56:45 -0300

    Add files via upload
    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+alert ('boas vindas ao jogo do numero secreto');
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute = prompt ('escolha um número entre 1 e 10');
+console.log(chute == numeroSecreto);
+
+if (numeroSecreto == chute) {
+    alert(`isso aí! você descobriu o número secreto: ${numeroSecreto}! Parabéns :)`);
+} else {
+    if(chute > numeroSecreto) {
+        alert(`O número secreto é menor que ${chute}`);
+        }    else{
+            alert(`O número secreto é maior que ${chute}`);
+        }
+    }
+
