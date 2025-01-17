872ad2831bed1177b20bb83ecb7e18834781c611
Henrique <henrique150789123@gmail.com>
2025-01-17 06:32:03 -0300

    adicionando função while

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 alert ('boas vindas ao jogo do numero secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute = prompt ('escolha um número entre 1 e 10');
-console.log(chute == numeroSecreto);
+let chute
 
-if (numeroSecreto == chute) {
-    alert(`isso aí! você descobriu o número secreto: ${numeroSecreto}! Parabéns :)`);
-} else {
-    if(chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}`);
-        }    else{
-            alert(`O número secreto é maior que ${chute}`);
+while (chute != numeroSecreto){
+    chute = prompt ('escolha um número entre 1 e 10');
+
+    if (numeroSecreto == chute) {
+        alert(`isso aí! você descobriu o número secreto: ${numeroSecreto}! Parabéns :)`);
+    } else {
+        if(chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+            }    else{
+                alert(`O número secreto é maior que ${chute}`);
+            }
         }
-    }
+    
+    
+}
 
