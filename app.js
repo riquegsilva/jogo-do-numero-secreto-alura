ec23658ad8e18c81965b2adfe11955f64486d7ab
Henrique <henrique150789123@gmail.com>
2025-01-20 16:47:28 -0300

    pegando valor de uma tag html para o javascript

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ exibirTextoNaTela('h1', 'Jogo do Número Secret');
 exibirTextoNaTela('p', 'Escolha um número de 1 a 10');
 
 function verificandoChute() {
-    console.log('botão clicado');
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
 }   
 
-//quando precisamos de um retorno da função, devemos usar o return. Como nesse caso nós esperamos o retorno do numero gerado, precisamos usar...
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 +1);    
 }
