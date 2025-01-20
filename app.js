25d1f63ef4f09e389761686afeb8a3deb79051eb
Henrique <henrique150789123@gmail.com>
2025-01-20 16:37:12 -0300

    gerando numero pseudo aleatório com uso de function

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -8,4 +10,9 @@ exibirTextoNaTela('p', 'Escolha um número de 1 a 10');
 
 function verificandoChute() {
     console.log('botão clicado');
-} 
+}   
+
+//quando precisamos de um retorno da função, devemos usar o return. Como nesse caso nós esperamos o retorno do numero gerado, precisamos usar...
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 +1);    
+}
